4f83248f8a45e491657818dbced1474a0fdbef26
Matheus Santana <matheussantanaengineer@gmail.com>
2021-03-16 20:45:12 -0300

    Criando Requests

--- src/controllers/clienteController.js
+++ src/controllers/clienteController.js
@@ -1,3 +1,4 @@
+
 // Define a utilização do model cliente e a dependência http-status
 const Cliente = require('../models/cliente');
 const status = require('http-status');
@@ -27,3 +28,79 @@ exports.Insert = (req, res, next) => {
         //catch = registra o que queremos que aconteca quando a Promise falhar
         .catch(error => next(error));
 };
+ 
+exports.SelectAll = (req, res, next) => {
+    Cliente.findAll()
+        .then(cliente => {
+            if (cliente) {
+                res.status(status.OK).send(cliente);
+            }
+        })
+        .catch(error => next(error));
+}
+ 
+exports.SelectDetail = (req, res, next) => {
+    const id = req.params.id;
+ 
+    Cliente.findByPk(id)
+        .then(cliente => {
+            if (cliente) {
+                res.status(status.OK).send(cliente);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+ 
+exports.Update = (req, res, next) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+    const salario = req.body.salario;
+    const dataNascimento = req.body.dataNascimento;
+    const ativo = req.body.ativo;
+ 
+    Cliente.findByPk(id)
+        .then(cliente => {
+            if (cliente) {
+                cliente.update({
+                    nome: nome,
+                    salario: salario,
+                    dataNascimento: dataNascimento,
+                    ativo: ativo
+                },
+                    {
+                        where: { id: id }
+                    })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+ 
+exports.Delete = (req, res, next) => {
+    const id = req.params.id;
+ 
+    Cliente.findByPk(id)
+        .then(cliente => {
+            if (cliente) {
+                cliente.destroy({
+                    where: { id: id }
+                })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            }
+            else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+ 
